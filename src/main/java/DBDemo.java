d4030dcbb23089ba4e11439a38ae874c9c9885d0
rksgr <r_kjha@yahoo.com>
2022-01-17 12:51:22 +0530

    Added: Introductory message

---
+++ src/main/java/DBDemo.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Driver;
+import java.util.Enumeration;
+
+public class DBDemo {
+    public static void main(String[] args) {
+        System.out.println("Today we shall solve use cases related to Employee Payroll using JDBC.");
+    }
+}
