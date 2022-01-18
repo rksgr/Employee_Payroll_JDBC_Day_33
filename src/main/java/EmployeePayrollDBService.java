4447fd80ad7f64f5d6af7ccbec19527900a767b5
rksgr <r_kjha@yahoo.com>
2022-01-18 14:08:03 +0530

    Added method to insert new entry into employee_payroll table

--- src/main/java/EmployeePayrollDBService.java
+++ src/main/java/EmployeePayrollDBService.java
@@ -6,6 +6,59 @@ import java.util.List;
 
 public class EmployeePayrollDBService {
 
+    private PreparedStatement employeePayrollDataStatement;
+    private static EmployeePayrollDBService employeePayrollDBService;
+
+    private EmployeePayrollDBService(){
+
+    }
+
+    public static EmployeePayrollDBService getInstance(){
+        if (employeePayrollDBService == null){
+            employeePayrollDBService = new EmployeePayrollDBService();
+            return employeePayrollDBService;
+        }
+        return employeePayrollDBService;
+    }
+
+    private void prepareStatementForEmployeeData() {
+        try{
+            Connection connection = this.getConnection();
+            String sql = "SELECT * FROM employee_payroll WHERE name =?";
+            employeePayrollDataStatement = connection.prepareStatement(sql);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public List<EmployeePayrollData> getEmployeePayrollData(String name){
+        List<EmployeePayrollData> employeePayrollList = null;
+        if (this.employeePayrollDataStatement == null){
+            this.prepareStatementForEmployeeData();
+        }
+        try{
+            employeePayrollDataStatement.setString(1,name);
+            ResultSet resultSet = employeePayrollDataStatement.executeQuery();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return employeePayrollList;
+    }
+    private List<EmployeePayrollData> getEmployeePayrollData(ResultSet resultSet){
+        List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+        try {
+            while (resultSet.next()) {
+                int emp_id = resultSet.getInt("emp_id");
+                String name = resultSet.getString("name");
+                double basic_pay = resultSet.getDouble("basic_pay");
+                LocalDate start_date = resultSet.getDate("start_date").toLocalDate();
+                employeePayrollList.add(new EmployeePayrollData(emp_id, name, basic_pay, start_date));
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return employeePayrollList;
+    }
+
     private Connection getConnection() throws SQLException{
         String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
         String userName = "root";
@@ -26,19 +79,24 @@ public class EmployeePayrollDBService {
             
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
-            while (resultSet.next()){
-                int emp_id = resultSet .getInt("emp_id");
-                String name = resultSet.getString("name");
-                double salary = resultSet.getDouble("basic_pay");
-                LocalDate start_date = resultSet.getDate("start_date").toLocalDate();
-                employeePayrollList.add(new EmployeePayrollData(emp_id,name,salary,start_date));
-            }
+            employeePayrollList = this.getEmployeePayrollData(resultSet);
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return employeePayrollList;
     }
-
-
+    public int updateEmployeeData(String name, double basic_pay){
+        return this.updateEmployeeDataUsingStatement(name,basic_pay);
+    }
+    private int updateEmployeeDataUsingStatement(String name,double basic_pay){
+        String sql = String.format("update employee_payroll set basic_pay= %f where name= '%s;',basic_pay,name");
+        try(Connection connection = this.getConnection()){
+            Statement statement = connection.createStatement();
+            return statement.executeUpdate(sql);
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
