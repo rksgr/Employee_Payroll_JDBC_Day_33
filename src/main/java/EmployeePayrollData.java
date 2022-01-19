056c93090034296059b2e45de976a8d496602681
rksgr <r_kjha@yahoo.com>
2022-01-19 12:30:02 +0530

    No change

--- src/main/java/EmployeePayrollData.java
+++ src/main/java/EmployeePayrollData.java
@@ -1,10 +1,11 @@
 import java.time.LocalDate;
+import java.util.Date;
 
 public class EmployeePayrollData {
     public int emp_id;
     public String name;
     public double basic_pay;
-    public LocalDate startDate;
+    public Date startDate;
 
     public EmployeePayrollData(int emp_id, String name, double salary){
         this.emp_id = emp_id;
@@ -12,13 +13,14 @@ public class EmployeePayrollData {
         this.basic_pay = salary;
     }
 
-    public EmployeePayrollData(int emp_id, String name, double salary, LocalDate startDate){
+    public EmployeePayrollData(int emp_id, String name, double salary, Date startDate){
         this(emp_id,name,salary);
         this.startDate = startDate;
     }
     public String toString(){
         return "emp_id="+ emp_id + ",+ name ='"+name+ '\'' +", salary= " + basic_pay ;
     }
+
     public boolean equals(Object o){
         if (this ==o)  return true;
         if (o== null || getClass() != o.getClass()) return false;
